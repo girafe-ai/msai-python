718cf8372e4fa8101d7dd5743534e4d9e22ca59f
Nick <niklevashov@yandex.ru>
2021-10-20 12:47:22 +0300

    Publish initial materials

--- week1_00_packages/package_project/resourse_example.py
+++
@@ -1,9 +0,0 @@
-# resource_example.py
-from importlib import resources
-import resource_package
-
-with resources.open_text(resource_package, "resource_file.txt") as res:
-    print(res.read())
-
-# Hello, this is resource text file!
-# Have a nice day!
--- week1_00_packages/package_project/subpackage_2/subpackage_3/module_2.py
+++
@@ -1,8 +0,0 @@
-# subpackage_3/module_2.py
-import os
-
-PATH = os.path.dirname(os.path.abspath(__file__))
-
-
-def module_2_func():
-    return PATH
