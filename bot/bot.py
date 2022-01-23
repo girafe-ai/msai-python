1dfe19c132cd59125ef64164cce007845ffb6cf8
Georgy Gunkin <FortMinors@yandex.ru>
2022-01-24 00:44:39 +0300

    refactor: changed to dealing with classes inside bot

--- bot/bot.py
+++ bot/bot.py
@@ -1,42 +1,46 @@
 import telebot
 import random
 
-bot = telebot.TeleBot("5188887338:AAEkmGdVyFHkIw4gt_-oiksnYBJuvdl3bY0")
+class Hangman:
+	def __init__(self):
+		self.bot = telebot.TeleBot("5188887338:AAEkmGdVyFHkIw4gt_-oiksnYBJuvdl3bY0")
 
-YesNoKeyboard = telebot.types.ReplyKeyboardMarkup(True,True)
-YesNoKeyboard.row("Yes", "No")
+		self.YesNoKeyboard = telebot.types.ReplyKeyboardMarkup(True,True)
+		self.YesNoKeyboard.row("Yes", "No")
 
-q = "\U00002753"
+		self.q = "\U00002753"
 
-words = list()
+		with open("words.txt", 'r') as file:
+			self.words = file.readlines()
 
-with open("words.txt", 'r') as file:
-	words = file.readlines()
+		@self.bot.message_handler(commands=['start', 'help'])
+		def Welcome(message):
+			reply = self.bot.send_message(message.chat.id, "Hey! This is a Hangman bot. Wanna play?", reply_markup=self.YesNoKeyboard)
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	reply = bot.send_message(message.chat.id, "Hey! This is a Hangman bot. Wanna play?", reply_markup=YesNoKeyboard)
+			# bot.send_message(message.chat.id, "".join("\U00002753" for i in range(5)).join("A").join("\U00002753" for i in range(5)))
 
-	# bot.send_message(message.chat.id, "".join("\U00002753" for i in range(5)).join("A").join("\U00002753" for i in range(5)))
+			self.bot.register_next_step_handler(reply, self.Handle)
 
-	bot.register_next_step_handler(reply, Handle)
+	def Handle(self, message):
+		word = random.choice(self.words)
+		
+		reply = self.bot.reply_to(message, f"Here is your word: {word}. Make a guess!")
 
-def Handle(message):
-	word = random.choice(words)
+		self.bot.register_next_step_handler(reply, self.CheckLetterHandler, word)
 
-	reply = bot.reply_to(message, f"Here is your word: {word}. Make a guess!")
+	def CheckLetterHandler(self, message, word):
+		if (self.CheckLetter(word, message.text)):
+			self.bot.send_message(message.chat.id, "Letter found!")
+		else:
+			self.bot.send_message(message.chat.id, "Letter not found!")
 
-	bot.register_next_step_handler(reply, CheckLetterHandler, word)
+	def CheckLetter(self, word, letter):
+		if letter in word:
+			return True
+		return False
 
-def CheckLetterHandler(message, word):
-	if (CheckLetter(word, message.text)):
-		bot.send_message(message.chat.id, "Letter found!")
-	else:
-		bot.send_message(message.chat.id, "Letter not found!")
+	def Run(self):
+		self.bot.infinity_polling()
 
-def CheckLetter(word, letter):
-	if letter in word:
-		return True
-	return False
-
-bot.infinity_polling()
+hangman = Hangman()
+hangman.Run()
